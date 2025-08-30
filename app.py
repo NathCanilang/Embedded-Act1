ead67baeec5d723032deb4261403a6229c52625d
Nathaniel Ephraim Canilang <nathcanilang@gmail.com>
2025-08-30 22:47:28 +0800

    debugging

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, Response, redirect, url_for, request, jsonify
 from dht11 import DHT11_Data, Sensor_Data
+from buzzer import BuzzerController
 app = Flask(__name__)
 
 dht11 = DHT11_Data()
+buzzer = BuzzerController()
 
 @app.route('/')
 def index():
@@ -17,6 +19,15 @@ def get_current_data():
         "temperature": sensor_data.temperature,
         "humidity": sensor_data.humidity    })
 
+@app.route('/start_buzzer', methods=["POST"])
+def start_buzzer():
+    buzzer.start()
+    return jsonify({"status": "buzzer started"})
+
+@app.route('/stop_buzzer', methods=["POST"])
+def stop_buzzzer():
+    buzzer.stop
+    return jsonify({"status": "buzzer stopped"})
 
 if __name__ == '__main__':
     app.run()
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,6 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const tempText = document.getElementById("temperatureDisplay");
     const humidityText = document.getElementById("humidityDisplay");
 
+    function startBuzzer() {
+    fetch('/start_buzzer', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => console.log(data.status))
+        .catch(err => console.error("Error:", err));
+    }
+
+    function stopBuzzer() {
+    fetch('/stop_buzzer', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => console.log(data.status))
+        .catch(err => console.error("Error:", err));
+    }
+
     function displayCurrentTime() {
         const currentTime = new Date();
         const formattedTime = currentTime.toLocaleTimeString();
@@ -14,8 +28,15 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 console.log("Received Data:", data);
-                tempText.textContent = "Temperature: " + data.temperature;
-                humidityText.textContent = "Humidity: " + data.humidity;
+                tempText.textContent = "Temperature: " + data.temperature + " °C";
+                humidityText.textContent = "Humidity: " + data.humidity + "%";
+
+                if(data.temperature <= 38.0){
+                    startBuzzer();
+                }
+                else{
+                    stopBuzzer();
+                }
             })
             .catch(error => console.log("Error fetching data: ", error));
     }
